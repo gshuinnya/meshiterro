5ff6484370f1b83f75d76df65c1a116247b67ee5
vagrant <vagrant@localhost.localdomain>
2020-07-06 06:26:57 +0000

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5c48bfc45a20699820367cfe895fd8e96d94bc76414c69d8cc79a5d76952fe2c8014c0a26fb99c85e58d30677d28b5dabd6b144fb20fd58733cea0e142f5f8b1'
